9c00f8fe3d9e378fac59e9ee404b09bf722ef050
peterajessup <peterajessup@users.noreply.github.com>
2025-12-08 12:56:07 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,10 @@
 oc project ace
 
 oc delete Configuration key.jks
-oc delete Configuration es-cert-ace.jks
-oc delete Configuration scramparms
-oc delete Configuration kafkapolicy
 oc delete Configuration httpsconf
-oc delete Configuration mqpolicy
-
-
-
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
 bash -x createConfig.sh keystore key.jks config/key.jks
-bash -x createConfig.sh truststore es-cert-ace.jks config/es-cert-ace.jks
-bash -x createConfig.sh setdbparms scramparms config/setdbparms.txt
-bash -x createConfig.sh policyproject kafkapolicy config/eventstreamspolicyproject.zip
-bash -x createConfig.sh policyproject mqpolicy config/MQPolicy.zip
+
 
 oc apply -f yaml/AceIVT-IR.yaml
-# oc apply -f yaml/aceIVTDeploy.yaml
+
