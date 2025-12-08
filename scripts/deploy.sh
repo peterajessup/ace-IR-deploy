f34407f0d2039f57c91d49e4002471ad6a60012b
peterajessup <peterajessup@users.noreply.github.com>
2025-12-08 13:00:55 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ oc delete Configuration key.jks
 oc delete Configuration httpsconf
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
 bash -x createConfig.sh keystore key.jks config/key.jks
-
+bash -x createConfig.sh dbparms dbparms config/setdbparms.txt
 
 oc apply -f yaml/AceIVT-IR.yaml
 
