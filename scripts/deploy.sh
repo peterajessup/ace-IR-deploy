a517113ecce567c15eb7afdf9f14faf2db3dcec6
peterajessup <peterajessup@users.noreply.github.com>
2025-12-08 15:58:22 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 oc project ace
 
 
-bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
-bash -x createConfig.sh keystore key.jks config/key.jks
-bash -x createConfig.sh setdbparms dbparms config/setdbparms.txt
+bash -x scripts/createConfig.sh serverconf httpsconf config/server.conf.yaml
+bash -x scripts/createConfig.sh keystore key.jks config/key.jks
+bash -x scripts/createConfig.sh setdbparms dbparms config/setdbparms.txt
 
 oc apply -f yaml/AceIVT-IR.yaml
 
