e72a0588039b917baca2d73026cd39666f0e5d02
peterajessup <peterajessup@users.noreply.github.com>
2025-12-08 15:45:56 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 oc project ace
 
-oc delete Configuration key.jks
-oc delete Configuration httpsconf
+
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
 bash -x createConfig.sh keystore key.jks config/key.jks
 bash -x createConfig.sh setdbparms dbparms config/setdbparms.txt
